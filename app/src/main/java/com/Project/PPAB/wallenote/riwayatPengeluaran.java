16f654a5c4b7a30f24ba2c4e3bdff47b2f6ef288
malihatmalam <malihatmalam@gmail.com>
2020-11-30 16:52:43 +0700

    Initial commit

---
+++ app/src/main/java/com/Project/PPAB/wallenote/riwayatPengeluaran.java
@@ -0,0 +1,14 @@
+package com.Project.PPAB.wallenote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class riwayatPengeluaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riwayat_pengeluaran);
+    }
+}
